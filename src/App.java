567c36d24c8f151ce1885d8aac9646156fc93392
SebastianSarasti <sebastiansarasti.14@gmail.com>
2023-11-19 09:42:26 -0500

    Encabezado y Series 8,9,10

--- src/App.java
+++ src/App.java
@@ -3,8 +3,17 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Grupo 4");
+        String red = "\033[31m";
+        String yellow = "\033[33m";
+        String cyan = "\033[36m";
+        String reset = "\u001B[0m";
+        System.out.println(cyan + "Hello, Grupo 4: CodeCrew" + reset);
+        System.out.println(red + "Integrantes: " + reset);
+        System.out.println(yellow + "Parra.Jhordy---Pastaz.Angel---Pisco.Christian---Proaño.Isaac");
+        System.out.println("Ramos.Sebastian---Rueda.Ruth---Sarasti.Sebastian---Zambrano.Jhair" + reset);
 
+        esperarTecla();
+        limpiarConsola();
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa el numero de elementos hasta el cual deseas hacer las secuencias ");
         int numeroVeces = Integer.parseInt(sc.nextLine());
@@ -23,16 +32,41 @@ public class App {
         oCodeCrewSerie8.mostrarSerie8DoWhile(numeroVeces);
         System.out.println("----Serie 8---While----");
         oCodeCrewSerie8.mostrarSerie8While(numeroVeces);
-        //Ejercicio 9
-        CodeCrewSerie9 oCodeCrewSerie9= new CodeCrewSerie9();
-        oCodeCrewSerie9.SerieFor(numeroVeces );
+        esperarTecla();
+        limpiarConsola();
+        // Ejercicio 9
+        CodeCrewSerie9 oCodeCrewSerie9 = new CodeCrewSerie9();
+        System.out.println("*************");
+        oCodeCrewSerie9.SerieFor(numeroVeces);
         System.out.println("*************");
         oCodeCrewSerie9.SeriDoWhile(numeroVeces);
         System.out.println("*************");
         oCodeCrewSerie9.SerieWhile(numeroVeces);
-        //Ejercicio 10
-        //Ejercicio 11
-        //Ejercicio 12
+        esperarTecla();
+        limpiarConsola();
+        // Ejercicio 10
+        CodeCrewSerie10 oCodeCrewSerie10 = new CodeCrewSerie10();
+        System.out.println("----Serie 10---For----");
+        oCodeCrewSerie10.mostrarSerie10For(numeroVeces);
+        System.out.println("----Serie 10---Do While----");
+        oCodeCrewSerie10.mostrarSerie10DoWhile(numeroVeces);
+        System.out.println("----Serie 10---While----");
+        oCodeCrewSerie10.mostrarSerie10While(numeroVeces);
+        esperarTecla();
+        limpiarConsola();
 
+        // Ejercicio 11
+        // Ejercicio 12
+
+    }
+
+    public static void esperarTecla() {
+        System.out.println("Presiona Enter para continuar...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine(); // Espera a que el usuario presione Enter
+    }
+
+    public static void limpiarConsola() throws Exception {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
 }
--- src/CodeCrewSerie9.java
+++ src/CodeCrewSerie9.java
@@ -6,41 +6,50 @@
  * Clase que genera una serie multiplicativa a partir de un número inicial.
  */
 public class CodeCrewSerie9 {
-public byte numeroInicio= 2;
-    byte numeroSiguiente=this.numeroInicio;
+    public int numeroInicio = 2;
+    int numeroSiguiente = this.numeroInicio;
+
     /**
      * Genera una serie multiplicativa utilizando un bucle for.
+     * 
      * @param numeroVeces Número de veces que se generará la serie.
      */
-    public void SerieFor(int numeroVeces){
-        byte numeroSiguiente= this.numeroInicio;
-        for (int i =0; i<numeroVeces; i++){
-            System.out.println(numeroSiguiente);
-            numeroSiguiente*=2;
+    public void SerieFor(int numeroVeces) {
+        int numeroSiguiente = this.numeroInicio;
+        for (int i = 0; i < numeroVeces; i++) {
+            System.out.print(numeroSiguiente);
+            numeroSiguiente *= 2;
         }
+        System.out.println();
     }
-     /**
+
+    /**
      * Genera una serie multiplicativa utilizando un bucle do-while.
+     * 
      * @param numeroVeces Número de veces que se generará la serie.
      */
-    public void SeriDoWhile(int numeroVeces){
-        byte i=1; 
+    public void SeriDoWhile(int numeroVeces) {
+        int i = 1;
         do {
-            System.out.println(numeroSiguiente);
-            numeroSiguiente*=2;
-        } while (i++<numeroVeces);
+            System.out.print(numeroSiguiente);
+            numeroSiguiente *= 2;
+        } while (i++ < numeroVeces);
+        System.out.println();
     }
+
     /**
      * Genera una serie multiplicativa utilizando un bucle while.
+     * 
      * @param numeroVeces Número de veces que se generará la serie.
      */
-    public void SerieWhile (int numeroVeces){
-        byte i=0;
-        numeroSiguiente= this.numeroInicio;
-        while (i++<numeroVeces) {
-            System.out.println(numeroSiguiente);
-             numeroSiguiente*=2;
+    public void SerieWhile(int numeroVeces) {
+        int i = 0;
+        numeroSiguiente = this.numeroInicio;
+        while (i++ < numeroVeces) {
+            System.out.print(numeroSiguiente);
+            numeroSiguiente *= 2;
         }
+        System.out.println();
     }
 
 }
