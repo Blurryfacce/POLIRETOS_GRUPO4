0acbac3be04dc8eae1672dac4ba9f61a6bf3f81d
Jhairzp_27 <jhairzp27@hotmail.com>
2023-11-19 15:52:08 -0500

    Prueba compilacion

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Grupo 4");
 
         //Ejercicio 1
         //Ejercicio 2
@@ -15,4 +14,5 @@ public class App {
         //Ejercicio 11
         //Ejercicio 12
     }
+
 }
