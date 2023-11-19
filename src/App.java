974e4309b754c49c7926f89d4ada6f43a5582c21
SebastianSarasti <sebastiansarasti.14@gmail.com>
2023-11-18 21:57:05 -0500

    reparticion de ejercicios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Grupo 4");
+
+        //Ejercicio 1
+        //Ejercicio 2
+        //Ejercicio 3
+        //Ejercicio 4
+        //Ejercicio 5
+        //Ejercicio 6
+        //Ejercicio 7
+        //Ejercicio 8
+        //Ejercicio 9
+        //Ejercicio 10
+        //Ejercicio 11
+        //Ejercicio 12
     }
 }
