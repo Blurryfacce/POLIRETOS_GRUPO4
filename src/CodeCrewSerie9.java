ded757dbe278592b817e40aa65910af81f8c8a17
sebas <sebastian.ramos01@epn.edu.ec>
2023-11-19 00:37:45 -0500

    C: documentacion ver 1 (serie 9)

--- src/CodeCrewSerie9.java
+++ src/CodeCrewSerie9.java
@@ -1,6 +1,17 @@
+/* Copyright (C) 2023, Sebastian_Ramos
+ * (CodeCrew) sebastian.ramos01@epn.edu.ec
+ * version 1.0
+ */
+/**
+ * Clase que genera una serie multiplicativa a partir de un número inicial.
+ */
 public class CodeCrewSerie9 {
 public byte numeroInicio= 2;
     byte numeroSiguiente=this.numeroInicio;
+    /**
+     * Genera una serie multiplicativa utilizando un bucle for.
+     * @param numeroVeces Número de veces que se generará la serie.
+     */
     public void SerieFor(int numeroVeces){
         byte numeroSiguiente= this.numeroInicio;
         for (int i =0; i<numeroVeces; i++){
@@ -8,6 +19,10 @@ public byte numeroInicio= 2;
             numeroSiguiente*=2;
         }
     }
+     /**
+     * Genera una serie multiplicativa utilizando un bucle do-while.
+     * @param numeroVeces Número de veces que se generará la serie.
+     */
     public void SeriDoWhile(int numeroVeces){
         byte i=1; 
         do {
@@ -15,6 +30,10 @@ public byte numeroInicio= 2;
             numeroSiguiente*=2;
         } while (i++<numeroVeces);
     }
+    /**
+     * Genera una serie multiplicativa utilizando un bucle while.
+     * @param numeroVeces Número de veces que se generará la serie.
+     */
     public void SerieWhile (int numeroVeces){
         byte i=0;
         numeroSiguiente= this.numeroInicio;
