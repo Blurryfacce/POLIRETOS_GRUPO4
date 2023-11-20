02075f469dcffde60e23f92c94b4e2acd7c3e072
AngelPastaz <85710526+Angelpstaz@users.noreply.github.com>
2023-11-20 18:12:18 -0500

    Add Bucle while & do while

--- src/CodeCrewSerie2.java
+++ src/CodeCrewSerie2.java
@@ -18,5 +18,49 @@ public class CodeCrewSerie2 {
         }
         System.out.println();
     }
+/**
+     * Este método imprime la serie 1 0 3 0 5 0 7... utilizando un bucle while.
+     * 
+     * @param numeroVeces parámetro que limita la serie a un número específico de
+     *                    elementos.
+     */
+    public void mostrarSerie2While(int numeroVeces) {
+        int i = 0;
 
+        while (i < numeroVeces) {
+            int num = i;
+            if (num%2==0) {
+                System.out.print(" 0 ");
+            }
+            else{
+                System.out.print(num);
+            }
+            i++;
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * Este método imprime la serie 1 0 3 0 5 0 7... utilizando un bucle do-while.
+     * 
+     * @param numeroVeces parámetro que limita la serie a un número específico de
+     *                    elementos.
+     */
+    public void mostrarSerie2DoWhile(int numeroVeces) {
+        int i = 0;
+
+        do {
+            int num = i;
+            if (num%2==0) {
+                System.out.print(" 0 ");
+            }
+            else{
+                System.out.print(num);
+            }
+            i++;
+        } while (i < numeroVeces);
+
+        System.out.println();
+    }
 }
