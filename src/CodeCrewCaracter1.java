05f75f78abbae9397c3b6dac7164eab4e616891f
sebas <sebastian.ramos01@epn.edu.ec>
2023-12-01 19:37:26 -0500

    update y documentación serieC1

--- src/CodeCrewCaracter1.java
+++ src/CodeCrewCaracter1.java
@@ -1,12 +1,18 @@
+/* Copyright (C) 2023, Sebastian_Ramos
+ * (CodeCrew) sebastian.ramos01@epn.edu.ec
+ * version 1.0
+ */
+/**
+ * Clase que genera la serie 1 de caracteres
+ */
 public class CodeCrewCaracter1 {
-
+/**
+ * Genera la serie 1, con el ciclo for.
+ * @param numeroVeces es la cantidad de caracteres que tendrá la serie 
+ */
     public void mostrarSerieCaracter1(int numeroVeces) {
         for (int contador = 0; contador < numeroVeces; contador++) {
-            if (contador % 2 == 0) {
-                System.out.print(" + ");
-            } else {
-                System.out.print(" - ");
-            }
+            System.out.print((contador%2==0) ? "+ ":"- " );
         }
     }
 }
