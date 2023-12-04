d8861f08fdbb10b5e116b93b3f0ad4e175b672f2
ChrisPisc <david.pisco@outlook.com>
2023-12-03 19:26:27 -0500

    Corrigiendo caracteres de las figuras 1 y 2

--- src/CodeCrewFigura1.java
+++ src/CodeCrewFigura1.java
@@ -3,7 +3,7 @@ public class CodeCrewFigura1 {
 
         for(int i = 1;i<=altura;i++){
             for(int j = 1; j<=altura; j++){
-                System.out.print((i==1||j==1||i==altura||j==altura) ? "+ ":"  " );
+                System.out.print((i==1||j==1||i==altura||j==altura) ? "* ":"  " );
 
             }
             System.out.println();
